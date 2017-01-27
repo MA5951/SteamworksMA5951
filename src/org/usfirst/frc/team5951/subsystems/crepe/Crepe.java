2d8727a221489c198cf78c1ef2d3a8ae4d3fe2f8
Omer Libai <omerlibai.2@gmail.com>
2017-01-27 12:00:16 +0200

    Added Crepe logic

--- src/org/usfirst/frc/team5951/subsystems/crepe/Crepe.java
+++ src/org/usfirst/frc/team5951/subsystems/crepe/Crepe.java
@@ -2,45 +2,52 @@ package org.usfirst.frc.team5951.subsystems.crepe;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 /**
+ * the crepe system opens and closes so it can take gears on the target.
  * 
  * @author Omer Libai
- *the crepe system opens and closes so it can take gears on the target.
- *
  */
 public class Crepe {
-	
-	//declaring the DoubleSolenoid
+
+	// Declaring variables
 	private DoubleSolenoid crepeSolenoid;
-	private boolean crepeOpen; //boolean of the current status of the crepe (is it open or closed?) 
+	private boolean isCrepeOpen; // boolean of the current status of the crepe
+									// (is it open or closed?)
+
 	/**
 	 * constructor for crepe.
 	 */
-	public Crepe(){
+	public Crepe() {
 		crepeSolenoid = CrepeComponents.crepeSolenoid;
-		crepeOpen = false;
+		isCrepeOpen = false;
 	}
+
 	/**
 	 * open's the crepe.
 	 */
-	public void openCrepe(){
+	public void openCrepe() {
 		crepeSolenoid.set(Value.kForward);
+		isCrepeOpen = true;
 	}
-	
+
 	/**
-	 * 
 	 * close's the crepe.
 	 */
-	public void closeCrepe(){
+	public void closeCrepe() {
 		crepeSolenoid.set(Value.kReverse);
+		isCrepeOpen = false;
 	}
+
 	/**
 	 * if the crepe is open, it closes. if it's closed, it opens it.
 	 */
-	public void toggleCrepe(){
-		if(crepeOpen)			
+	public void toggleCrepe() {
+		if (isCrepeOpen) {
 			this.closeCrepe();
-		if(!crepeOpen)
-			this.openCrepe();
+			return;
+		}
+
+		this.openCrepe();
 	}
 }
